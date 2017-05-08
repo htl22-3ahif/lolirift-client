a6ac61abacd9581d5bb89ece707196c9b9cba0bb
noby <nobytrem@gmail.com>
2017-05-08 12:00:25 +0200

    added sexy log sigmoid function for fancy zooming

--- app/components/CoordinateSystem.js
+++ app/components/CoordinateSystem.js
@@ -17,7 +17,7 @@ export default class CoordinateSystem extends React.Component {
     this.state = {
       width: window.innerWidth,
       height: window.innerHeight,
-      origin: { x: 0, y: 0 },
+      origin: { x: window.innerWidth / 2, y: window.innerHeight / 2 },
       move: false,
       oldMouse: { x: 0, y: 0 },
       stride: { x: 40, y: 40 },
@@ -85,27 +85,7 @@ export default class CoordinateSystem extends React.Component {
       }
     }
 
-    // code for draw tile, inserted here for testing
-    {
-      ctx.fillStyle = '#0080ff'
-
-      // basically, undo the calculations made in onClick (where the selected grid
-      // element is claculated)
-      var offsetX = this.state.origin.x % this.state.stride.x
-      var originX = Math.floor(this.state.origin.x / this.state.stride.x)
-      var offsettedPageX = (this.state.selectedGrid.x - originX) * this.state.stride.x
-      offsettedPageX = offsettedPageX - offsetX
-
-      var offsetY = this.state.origin.y % this.state.stride.y
-      var originY = Math.floor(this.state.origin.y / this.state.stride.y)
-      var offsettedPageY = (this.state.selectedGrid.y - originY) * this.state.stride.y
-      offsettedPageY = offsettedPageY - offsetY
-
-      ctx.fillRect(offsettedPageX, offsettedPageY, this.state.stride.x, this.state.stride.y)
-
-      ctx.fillText("tiles: " + this.state.selectedGrid.x + ", " + this.state.selectedGrid.y, 10, 95)
-      ctx.fillText("offseted page: " + offsettedPageX + ", " + offsettedPageY, 10, 110)
-    }
+    this.drawTile(this.state.selectedGrid.x, this.state.selectedGrid.y, '#0080ff')
 
     // write some information on the screen
     {
@@ -113,6 +93,9 @@ export default class CoordinateSystem extends React.Component {
       ctx.fillText("mouse pos: " + this.state.oldMouse.x + ", " + this.state.oldMouse.y, 10, 10)
       ctx.fillText("origin: " + this.state.origin.x + ", " + this.state.origin.y, 10, 25)
       ctx.fillText("selected grid element: " + this.state.selectedGrid.x + ", " + this.state.selectedGrid.y, 10, 40)
+      ctx.beginPath()
+      ctx.arc(this.state.origin.x, this.state.origin.y, 3, 0, 2 * Math.PI, false)
+      ctx.stroke()
     }
   }
 
@@ -137,40 +120,18 @@ export default class CoordinateSystem extends React.Component {
     const ctx = this.refs.canvas.getContext('2d')
     ctx.fillStyle = color
 
-    var offsetX = this.state.origin.x % this.state.stride.x
-    var originX = Math.floor(this.state.origin.x / this.state.stride.x)
-    var offsettedPageX = (tileX - originX) * this.state.stride.x
-    offsettedPageX = this.state.width - offsettedPageX - offsetX
+    var x = tileX * this.state.stride.x + this.state.origin.x
+    var y = tileY * this.state.stride.y + this.state.origin.y
 
-    var offsetY = this.state.origin.y % this.state.stride.y
-    var originY = Math.floor(this.state.origin.y / this.state.stride.y)
-    var offsettedPageY = (tileY - originY) * this.state.stride.y
-    offsettedPageY = this.state.height - offsettedPageY - offsetY
-
-    ctx.fillRect(offsettedPageX, offsettedPageY, this.state.stride.x, this.state.stride.y)
+    ctx.fillRect(x, y, this.state.stride.x, this.state.stride.y)
   }
 
   onClick = (e) => {
-    // get the offset the grid has to the edge of the screen (relevant for pageX)
-    var offsetX = this.state.origin.x % this.state.stride.x
-    var offsettedPageX = e.pageX + offsetX
-    // finally, calculate the selected spot on the grid
-    // floor the result to get an integer
-    var gridX = Math.floor(
-      // take the position and divide by our stride value to get xth grid element
-      // our pageX offset is negated in order to make pageX "flow" with the rate
-      // of change of our origin, as further down means a decreasing value, which
-      // is not the case with page values
-      ((offsettedPageX) + this.state.origin.x) / this.state.stride.x
-    )
+    var mousePosRelativeToOriginX = e.pageX - this.state.origin.x
+    var gridX = Math.floor(mousePosRelativeToOriginX / this.state.stride.x)
 
-    var offsetY = this.state.origin.y % this.state.stride.y
-    var offsettedPageY = e.pageY + offsetY
-    var gridY = Math.floor(
-      ((offsettedPageY) + this.state.origin.y) / this.state.stride.y
-    )
-
-    //this.drawTile(gridX, gridY, '#0080ff')
+    var mousePosRelativeToOriginY = e.pageY - this.state.origin.y
+    var gridY = Math.floor(mousePosRelativeToOriginY / this.state.stride.y)
 
     this.setState({
       selectedGrid: {
@@ -199,8 +160,8 @@ export default class CoordinateSystem extends React.Component {
   onMouseMove = (e) => {
     if (this.state.move) {
       var relative = {
-        x: this.state.oldMouse.x - e.pageX,
-        y: this.state.oldMouse.y - e.pageY
+        x: e.pageX - this.state.oldMouse.x,
+        y: e.pageY - this.state.oldMouse.y
       }
       var oldOrigin = this.state.origin
       this.setState({
@@ -218,27 +179,44 @@ export default class CoordinateSystem extends React.Component {
 
   // scrolling will change stride size (and therefore scale the grid)
   onWheel = (e) => {
-    // using a logistic growth function, the rate of change will decrease the closer
-    // it is to its boundaries (K = 100, y0 = 0)
-    // k = 10, K = 100, y0 = 0
-    /* var k = 10
-    var K = 100
-    var y0 = 0
-    var t = (this.state.stride.x + this.state.stride.y) / 2 */
-    // restricted growth function
-    //var growth = K + (y0 - K) * Math.pow(Math.E, -(k / K) * t)
-    // derivative of our restricted growth function
-    //var derivative = k * (1 - (growth / K)) dis not work
+    // graph height, shift, tightness
+    var h = 1000
+    var s = 250
+    var t = 80
 
-    //logistic growth function
-    //var growth = (y0 * K) / (y0 + (K - y0) * Math.pow(Math.E, -k * t))
-    //derivative
-    //var derivative = k * growth * (1 - (growth / K)) dis not work either
+    // continuous log-sigmoid function
+    var x = this.state.stride.x
+    var y = this.state.stride.y
+
+    // sigmoid function
+    //var sigmoidX = h / (1 + Math.pow(Math.E, -(1 / 20) * (x - s)))
+    //var sigmoidY = h / (1 + Math.pow(Math.E, -(1 / 20) * (y - s)))
+
+    // derivative of sigmoid function
+
+    // recurring euler values for derivative
+    var ex = Math.pow(Math.E, -(1 / t) * (x - s))
+    var ey = Math.pow(Math.E, -(1 / t) * (y - s))
+
+    // derivative
+    var dSigmoidX = ex / (Math.pow(ex, 2) + (2 * ex) + 1)
+    var dSigmoidY = ey / (Math.pow(ey, 2) + (2 * ey) + 1)
+
+    // increase
+
+    var ix = (dSigmoidX * e.deltaY / 10)
+    var iy = (dSigmoidY * e.deltaY / 10)
+
+    // result
+    var newStrideX = ((this.state.stride.x + ix) > 10) ? this.state.stride.x + ix : 10
+    newStrideX = ((newStrideX) < 190) ? newStrideX : 190
+    var newStrideY = ((this.state.stride.y + iy) > 10) ? this.state.stride.y + iy : 10
+    newStrideY = ((newStrideY) < 190) ? newStrideY : 190
 
     this.setState({
       stride: {
-        x: this.state.stride.x + (e.deltaY / 10 * -1 * this.state.rate),
-        y: this.state.stride.y + (e.deltaY / 10 * -1 * this.state.rate)
+        x: newStrideX,
+        y: newStrideY
       }
     })
   }
