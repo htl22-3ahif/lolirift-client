2ba0fe2058dda826962f56bea0771ad9183d01a7
satorialist <nobytrem@gmail.com>
2017-05-26 20:44:48 +0200

    minor changes

---
+++ app/components/Unit/UnitSelection.js
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class Navigation extends Component {
+
+  static propTypes = {}
+
+  constructor () {
+    super()
+  }
+
+  componentDidMount () {}
+
+  render () {
+    
+  }
+}
